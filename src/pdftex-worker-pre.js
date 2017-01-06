3b85aba8ef5c50269806927c3abb833c8e222bea
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2017-01-06 16:59:03 +0100

    dtrn

--- src/pdftex-worker-pre.js
+++ src/pdftex-worker-pre.js
@@ -6,29 +6,78 @@ Module.noInitialRun = true
 Module.print = function (log) { postMessage({ type: 'log', value: log }) }
 Module.printErr = function (err) { postMessage({ type: 'err', value: err }) }
 
+function start (source, options) {
+  options = options || {}
+  if (options.enableUrls == null) {
+    options.enableUrls = true
+  }
+  if (options.proxy == null) {
+    options.proxy = 'https://proxyl2p-yayay.rhcloud.com/'
+  }
+  options.fileList = options.fileList || []
+
+  // register url files
+  if (options.enableUrls) {
+    var files = source.match(/\{(https|http):\/\/.*\}/g)
+    files.forEach(function (f, i) {
+      var url = f.slice(1,-1) // remove {}
+      var dotPos = url.lastIndexOf('.')
+      var extension
+      if (dotPos >= 0) {
+        extension = url.slice(dotPos)
+      } else {
+        extension = ''
+      }
+      var filename = 'remote' + i + extension
+      if (url.indexOf(location.origin) !== 0 && options.proxy !== false) {
+        url = options.proxy + url
+      }
+      Module.FS_createLazyFile('/', filename, url, true, true)
+      source = source.replace(new RegExp(f, 'g'), '{/' + filename + '}')
+    })
+  }
+
+  // register files in fileList
+  options.fileList.forEach(function (f) {
+    var from = f[0]
+    var url = f[1]
+    // ensure path to file exists
+    from = from.split('/')
+    var filename = from.pop()
+    var dir = from.join('/')
+    if (dir[0] !== '/') {
+      dir = '/' + f
+    }
+    FS.createPath(dir)
+    Module.FS_createLazyFile(dir, filename, url, true, true)
+  })
+
+  try {
+    shouldRunNow = true
+    FS.writeFile('input.tex', source)
+    Module.run(['-interaction=nonstopmode', '-output-format', 'pdf', 'input.tex'])
+  } catch (err) {
+    // log
+    uint8Array = FS.readFile('input.log')
+    var log = new TextDecoder("utf-8").decode(uint8Array)
+    postMessage({ type: 'finish', value: { success: false, message: err + '', url: null, log: log } })
+    return
+  }
+  // pdf
+  uint8Array = FS.readFile('input.pdf')
+  var blob = new Blob([uint8Array], { type: 'application/pdf' })
+  var url = URL.createObjectURL(blob)
+  // log
+  uint8Array = FS.readFile('input.log')
+  var log = new TextDecoder("utf-8").decode(uint8Array)
+  postMessage({ type: 'finish', value: {success: true, url: url, log: log }})
+}
+
 addEventListener('message', function (event) {
   var m = event.data
   var uint8Array
   if (m.type === 'start') {
-    try {
-      shouldRunNow = true
-      FS.writeFile('input.tex', m.source)
-      Module.run(['-interaction=nonstopmode', '-output-format', 'pdf', 'input.tex'])
-    } catch (err) {
-      // log
-      uint8Array = FS.readFile('input.log')
-      var log = new TextDecoder("utf-8").decode(uint8Array)
-      postMessage({ type: 'finish', value: { success: false, message: err + '', url: null, log: log } })
-      return
-    }
-    // pdf
-    uint8Array = FS.readFile('input.pdf')
-    var blob = new Blob([uint8Array], { type: 'application/pdf' })
-    var url = URL.createObjectURL(blob)
-    // log
-    uint8Array = FS.readFile('input.log')
-    var log = new TextDecoder("utf-8").decode(uint8Array)
-    postMessage({ type: 'finish', value: {success: true, url: url, log: log }})
+    start(m.source, m.options)
   }
 })
 
